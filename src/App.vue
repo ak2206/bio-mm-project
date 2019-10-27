62a13c884273bc7d07b80d78f94868164724585f
aphymi <aphymi@gmail.com>
2019-10-27 15:49:54 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+	<div id="app">
+		<h1>Targeted Gene Therapy</h1>
+		
+		<p>
+			A significant number of diseases in humans are caused by adverse mutations in specific genes that prevent cells from carrying out their functions, or make them do so incorrectly. In response, targeted gene therapies have been created to combat the source of such diseases
+		</p>
+		
+		<hr/>
+		
+		<div id="disease-card-list">
+			<disease-card
+				v-for="disease in diseaseList"
+				:key="disease.name"
+				:diseaseName="disease.name"
+				:diseaseDescription="disease.description"
+			/>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	import axios from "axios";
+	import Vue from 'vue';
+	
+	import DiseaseCard from "@/components/DiseaseCard.vue";
+	
+	const diseaseListPromise = axios.get("/diseases.json");
+
+	export default Vue.extend({
+		name: 'app',
+		
+		components: {
+			"DiseaseCard": DiseaseCard,
+		},
+		
+		data() {
+			return {
+				diseaseList: [],
+			};
+		},
+		
+		mounted() {
+			diseaseListPromise.then((response) => {
+				this.diseaseList = response.data;
+			})
+		},
+	});
+</script>
+
+<style lang="scss">
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+	
+	html, body {
+		width: 100%;
+		min-height: 100%;
+	}
+	
+	body {
+		background-color: #d7fcd4;
+	}
+	
+	#disease-card-list {
+		display: flex;
+		flex-flow: row wrap;
+		
+		> * {
+			margin-right: 1.5em;
+			margin-bottom: 1.5em;
+		}
+	}
+</style>
---
+++ src/components/DiseaseCard.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="disease-card">
+		<h2>{{ diseaseName }}</h2>
+		<p>
+			{{ diseaseDescription }}
+		</p>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from "vue";
+
+	export default Vue.extend({
+		props: {
+			diseaseName: {
+				type: String,
+				required: true,
+			},
+			
+			diseaseDescription: {
+				type: String,
+				required: true,
+			}
+		}
+	});
+</script>
+
+<style lang="scss">
+	.disease-card {
+		width: 35ch;
+		background-color: #b6cca1;
+		
+		border: solid #b68f40 .3em;
+		border-radius: 2em;
+		
+		padding: .7em;
+	}
+</style>
